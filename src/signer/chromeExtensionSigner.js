c8568baa1ac846d180da551213846142eace733a
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-06-03 12:19:24 +0200

    add generic message function

--- src/signer/chromeExtensionSigner.js
+++ src/signer/chromeExtensionSigner.js
@@ -2,38 +2,63 @@ const byteArray2hexStr = require("../utils/bytes").byteArray2hexStr;
 
 const TRONSCAN_TRANSACTION = "TRONSCAN_TRANSACTION";
 const TRONSCAN_TRANSACTION_RESPONSE = "TRONSCAN_TRANSACTION_RESPONSE";
+const TRONSCAN_PING = "TRONSCAN_PING";
+const TRONSCAN_PONG = "TRONSCAN_PONG";
+const TRONSCAN_REQUEST_ACCOUNT = "TRONSCAN_REQUEST_ACCOUNT";
 
 class ChromeExtensionSigner {
+
+
   constructor() {
     this.callbackId = 0;
   }
 
-  signTransaction(transaction) {
+  async isExtensionAvailable() {
+    let check = await this.sendMessage({
+      type: TRONSCAN_PING,
+    });
 
+    return check.type === TRONSCAN_PONG;
+  }
+
+  async getAccount() {
+    let {account} = await this.sendMessage({
+      type: TRONSCAN_REQUEST_ACCOUNT,
+    });
+
+    return account;
+  }
+
+  sendMessage(data) {
+    let callbackId = this.callbackId++;
     return new Promise(resolve => {
-      let callbackId = this.callbackId++;
 
       let responseCallback = (event) => {
-        if (event.data.type && (event.data.type === TRONSCAN_TRANSACTION_RESPONSE)) {
-          if (event.data.callbackId === callbackId) {
-            window.removeEventListener("message", responseCallback);
-            resolve({
-              hex: event.data.transaction.hex,
-            });
-          }
+        if ((event.data._fromClient !== true) && (event.data.callbackId === callbackId)) {
+          window.removeEventListener("message", responseCallback);
+          resolve(event.data);
         }
       };
 
       window.addEventListener("message", responseCallback);
 
-      window.postMessage({
-        type: TRONSCAN_TRANSACTION,
+      window.postMessage(Object.assign({
         callbackId: callbackId,
-        transaction: {
-          hex: byteArray2hexStr(transaction.serializeBinary()),
-        },
-      }, "*");
+        _fromClient: true,
+      }, data), "*");
     });
+  }
+
+  async signTransaction(transactionObj) {
+
+    let {transaction} = await this.sendMessage({
+      type: TRONSCAN_TRANSACTION,
+      transaction: {
+        hex: byteArray2hexStr(transactionObj.serializeBinary()),
+      }
+    });
+
+    return transaction;
   }
 }
 
